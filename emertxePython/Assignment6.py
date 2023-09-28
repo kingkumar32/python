271b1831d7ca3d5c2772df097419997117f850f6
yashwanthprabaagaran <yashwanthprabaagaran1959@gmail.com>
2023-09-28 12:42:36 +0530

    Assignment 6

---
+++ emertxePython/Assignment6.py
@@ -0,0 +1,17 @@
+""""NAME : SUGUMAR P
+ID : 23012_017
+DATE : 28/09/2023
+DESCRIPTION : Write a program to print the following pattern
+SAMPLE IP:  Enter the number of rows, n=3 
+
+SAMPLE OP:  1
+            2 2
+            3 3 3 
+"""
+row=eval(input())
+
+for i in range(row):
+    for j in range(row):
+        if j<=i:
+            print(i+1,end=" ")        
+    print()
