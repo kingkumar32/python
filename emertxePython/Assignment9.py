7616fafb99230785a8b28a56dd9bbbee5033d3dc
yashwanthprabaagaran <yashwanthprabaagaran1959@gmail.com>
2023-09-28 12:59:16 +0530

    Assignment 9

---
+++ emertxePython/Assignment9.py
@@ -0,0 +1,13 @@
+""""NAME : SUGUMAR P
+ID : 23012_017
+DATE : 28/09/2023
+DESCRIPTION : Write a program to find the sum of all the elements in the list
+SAMPLE IP:  List = [12, 15, 3, 10] 
+SAMPLE OP: The sum of elements in the given list is 40 
+"""
+'''
+val=[eval(i) for i in input().split()]
+print("The sum of elements in the given list is ",sum(val))'''
+
+val=eval(input())
+print("The sum of elements in the given list is ",sum(val))
